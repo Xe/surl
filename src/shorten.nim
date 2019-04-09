caf0b05e92c6579895215b40d7df7fa682f81ae7
Christine Dodrill <me@christine.website>
2019-04-09 06:54:38 -0700

    shorten: use X-API-Options: bare

--- src/shorten.nim
+++ src/shorten.nim
@@ -1,11 +1,14 @@
 import httpclient, os
 
 for i in 1.countup(paramCount()):
+  let client = newHttpClient()
+  client.headers = newHttpHeaders({ "X-API-Options": "bare" })
+
   var data = newMultipartData()
   data["url"] = i.paramStr()
 
   try:
-    echo "https://g.o/submit".postContent(multipart=data)
+    echo client.postContent("https://g.o/submit", multipart=data)
 
   except:
-    echo getCurrentExceptionMsg()
+    quit getCurrentExceptionMsg()
